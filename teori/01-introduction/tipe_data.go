f659d708849c4f273e9f83f913a1a174e02c3580
iki121 <rizkyfadhlurrahmanramadhan@gmail.com>
2026-09-21 13:22:00 +0700

    feat(teori): add helloworld and tipe_data examples in 01-introduction

---
+++ teori/01-introduction/tipe_data.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var umur int = 26
+	var anakke int = 1
+	var saldo int64 = 1000000000
+
+	umur = 26
+	anakke = 1
+	saldo = 1000000000
+
+	fmt.Println("Umur saya:", umur)
+	fmt.Println("Saya anak ke:", anakke)
+	fmt.Println("Saldo saya:", saldo)
+}
